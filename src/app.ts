9585cbf4c00b7fdec92bc2104ef6b8052a168277
Naharun <naharunsraboni@gmail.com>
2024-11-25 00:45:30 +0600

    updated app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,3 @@
-// import cors from 'cors';
-// import express, { Application, Request, Response } from 'express';
-// import globalErrorHandler from './app/middlewares/globalErrorHandler';
-// import notFound from './app/middlewares/notFound';
-// import { AuthRoutes } from './app/modules/Auth/auth.route';
-// import { FacilityRoutes } from './app/modules/facility/facility.route';
-// import { BookingRoutes } from './app/modules/booking/booking.route';
-
-// const app: Application = express();
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-// // Application routes
-// app.use('/api/auth', AuthRoutes);
-// app.use('/api/facility', FacilityRoutes);
-// app.use('/api', BookingRoutes);
-
-// // Root route
-// app.get('/', (req: Request, res: Response) => {
-//   res.send('Hello World!');
-// });
-
-// // Global error handler
-// app.use(globalErrorHandler);
-
-// // 404 Not Found handler
-// app.use(notFound);
-
-// export default app;
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
@@ -40,7 +9,7 @@ import { BookingRoutes } from './app/modules/booking/booking.route';
 const app: Application = express();
 
 // Middleware configuration for CORS
-const allowedOrigins = ['https://sports-facilities-booking-client.vercel.app/']; // Add your frontend origin
+const allowedOrigins = ['https://sports-facilities-booking-client.vercel.app/'];
 
 const corsOptions = {
   origin: allowedOrigins,
